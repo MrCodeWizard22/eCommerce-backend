92fcfbccd570f78bea00816bdf95320d40e01f78
Piyush Varshney <piyushvarshney54@gmail.com>
2025-02-01 23:11:42 +0530

    first commit

---
+++ src/main/java/com/varshneys/ecommerce/ecommerce_backend/Model/User.java
@@ -0,0 +1,17 @@
+package com.varshneys.ecommerce.ecommerce_backend.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+}
